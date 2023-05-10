b08db554c806c1fc4a1cbc1b5ab8ead5a5e30c80
Xu_no_ghost <xu_no_ghost@163.com>
2023-05-10 16:58:42 +0800

    done lecture3.1

---
+++ Lect/3.1_Testing/Sort.java
@@ -0,0 +1,30 @@
+public class Sort {
+    public static void sort(String[] x) {
+        sort(x, 0);
+    }
+
+    private static void sort(String[] x, int start) {
+        if (start == x.length) {
+            return;
+        }
+        int smallestIndex = findSmallest(x, start);
+        swap(x, start, smallestIndex);
+        sort(x, start + 1);
+    }
+
+    public static int findSmallest(String[] x, int start) {
+        int res = start;
+        for (int i = start; i < x.length; i ++ ) {
+            if (x[i].compareTo(x[res]) < 0) {
+                res = i;
+            }
+        }
+        return res;
+    }
+
+    public static void swap(String[] x, int a, int b) {
+        String temp = x[a];
+        x[a] = x[b];
+        x[b] = temp;
+    }
+}
---
+++ Lect/3.1_Testing/TestSort.java
@@ -0,0 +1,50 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestSort {
+    @Test
+    public void testSort() {
+        String[] input = {"i", "have", "an", "egg"};
+        String[] expected = {"an", "egg", "have", "i"};
+
+        Sort.sort(input);
+/*
+        for (int i = 0; i < input.length; i ++ ) {
+            if (!input[i].equals(expected[i])) {
+                System.out.println("There is a mistake in position :" + i + " , expected : " +
+                        expected[i] + ", your answer is : " + input[i]);
+                break;
+            }
+        }
+*/
+
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testFindSmallest() {
+        String[] input = {"i", "have", "an", "egg"};
+        int expected = 2;
+
+        int actual = Sort.findSmallest(input, 0);
+
+        org.junit.Assert.assertEquals(expected, actual);
+
+        String[] input2 = {"i", "have", "an", "egg"};
+        int expected2 = 2;
+
+        int actual2 = Sort.findSmallest(input2, 2);
+
+        assertEquals(expected2, actual2);
+    }
+
+    @Test
+    public void testSwap() {
+        String[] input = {"i", "have", "an", "egg"};
+        int a = 0, b = 2;
+        String[] expected = {"an", "have", "i", "egg"};
+        Sort.swap(input, a, b);
+
+        assertArrayEquals(expected, input);
+    }
+}
